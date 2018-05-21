b1907e91d57314c08bca05fe14763e4a9b8c0afd
Sirui Lu <lsrcz@pku.edu.cn>
2018-05-21 13:15:39 +0800

    add local entropy

--- test/task1.py
+++ test/task1.py
@@ -1,19 +1,14 @@
+import time
+
 from PIL import Image
 import numpy as np
-import numexpr as ne
 import matplotlib.pyplot as plt
-import cv2
-import os
-#from multiprocessing import Pool
-import time
+plt.ion()
+
 import torch
 import torch.nn.functional as F
 
-# NUM_OF_PROCESS = 8
-# pool = Pool(processes=NUM_OF_PROCESS)
-plt.ion()
-#ne.set_num_threads(ne.detect_number_of_cores() // 2)
-
+# gradient with pytorch
 def computeGDTorch(npimg):
     tmp = npimg.astype(np.float32)
     tmp = torch.from_numpy(tmp)
@@ -30,14 +25,14 @@ def computeGDTorch(npimg):
     lurd = torch.abs(tmp[0:x - 1, 0:y - 1, :] - tmp[1:x, 1:y, :])
     ldru = torch.abs(tmp[1:x, 0:y - 1, :] - tmp[0:x - 1, 1:y, :])
 
-    gdsum += F.pad(V,(0,0,0,0,1,0),"constant",0)
-    gdsum += F.pad(V,(0,0,0,0,0,1),"constant",0)
-    gdsum += F.pad(H,(0,0,0,1,0,0), 'constant', 0)
-    gdsum += F.pad(H,(0,0,1,0,0,0), 'constant', 0)
-    gdsum += F.pad(lurd,(0,0,0,1,0,1), 'constant', 0)
-    gdsum += F.pad(lurd,(0,0,1,0,1,0), 'constant', 0)
-    gdsum += F.pad(ldru, (0,0,0, 1, 0, 1), 'constant', 0)
-    gdsum += F.pad(ldru, (0,0,1, 0, 1, 0), 'constant', 0)
+    gdsum += F.pad(V, (0, 0, 0, 0, 1, 0), "constant", 0)
+    gdsum += F.pad(V, (0, 0, 0, 0, 0, 1), "constant", 0)
+    gdsum += F.pad(H, (0, 0, 0, 1, 0, 0), 'constant', 0)
+    gdsum += F.pad(H, (0, 0, 1, 0, 0, 0), 'constant', 0)
+    gdsum += F.pad(lurd, (0, 0, 0, 1, 0, 1), 'constant', 0)
+    gdsum += F.pad(lurd, (0, 0, 1, 0, 1, 0), 'constant', 0)
+    gdsum += F.pad(ldru, (0, 0, 0, 1, 0, 1), 'constant', 0)
+    gdsum += F.pad(ldru, (0, 0, 1, 0, 1, 0), 'constant', 0)
 
     '''
     gdsum[0:x - 1, :, :] += V
@@ -53,8 +48,7 @@ def computeGDTorch(npimg):
     gdsum = gdsum / time
     return gdsum
 
-
-
+# We didn't use the implementation since it's slower than pytorch implementation
 def computeGD(npimg):
     tmp = npimg
     tmp = tmp.astype('double')
@@ -86,6 +80,39 @@ def showGDimg(gdimg):
     plt.imshow(Image.fromarray((gdimg / gdimg.max() * 255).astype(np.uint8)))
     plt.show()
 
+# local entropy with scipy
+from scipy.ndimage import generic_filter
+from scipy.stats import entropy
+def _entropy(values):
+    probabilities = np.bincount(values.astype(np.int)) / float(len(values))
+    return entropy(probabilities)
+def localEntropyScipy(img):
+    return generic_filter(img.astype(np.float), _entropy, size=9)
+
+# local entropy with skimage
+import skimage.filters.rank as sfr
+from skimage.morphology import square
+def localEntropySkimage(img):
+    return sfr.entropy(img, square(9))
+
+def generatePathTorch(gdimg):
+    lastDir = torch.zeros_like(gdimg, dtype=torch.uint8)
+    energy = torch.zeros_like(gdimg)
+    energy[0] = gdimg[0]
+    tmp = torch.ones((3,gdimg.shape[1])) * np.inf
+    t = torch.arange(tmp.shape[1])
+
+    for i in range(1,gdimg.shape[0]):
+        tmp[0,1:] = energy[i-1,:-1]
+        tmp[1] = energy[i-1]
+        tmp[2,:-1] = energy[i-1,1:]
+        #tmp[0] = F.pad(energy[i-1,:-1], (1,0),'constant',np.inf)
+        #tmp[1] = energy[i - 1]
+        #tmp[2] = F.pad(energy[i-1,1:], (0,1), 'constant', np.inf)
+        tmp += gdimg[i]
+        energy[i], lastDir[i] = torch.min(tmp, dim=0)
+        #lastDir[i] = torch.argmin(tmp, dim=0)
+    return energy.numpy(), lastDir.numpy()
 
 def generatePath(gdimg):
     lastDir = np.zeros_like(gdimg, dtype=np.uint8)
@@ -103,10 +130,14 @@ def generatePath(gdimg):
         #energy[i] = tmp[lastDir[i],t]
     return energy, lastDir
 
-
+USING_NUMPY = False
 def deleteOneRow(npimg):
-    gdimg = computeGDTorch(npimg).numpy()
-    energy, lastDir = generatePath(gdimg)
+    if USING_NUMPY:
+        gdimg = computeGD(npimg)
+        energy, lastDir = generatePath(gdimg)
+    else:
+        gdimg = computeGDTorch(npimg)
+        energy, lastDir = generatePathTorch(gdimg)
     #mask = np.full(gdimg.shape, True)
 
     last = np.argmin(energy[-1])
@@ -117,10 +148,16 @@ def deleteOneRow(npimg):
         last = last + lastDir[i][last] - 1
     return ret #npimg[mask].reshape((gdimg.shape[0], gdimg.shape[1] - 1, 3))
 
+__name__ = 'test'
+if __name__ == 'test':
+    img = Image.open('../pics/ori.jpg').convert('L')
+    x = localEntropySkimage(np.array(img))
+    showGDimg(x)
+
 
 if __name__ == '__main__':
     print('start, ', time.asctime(time.localtime(time.time())))
-    img = Image.open('./pics/bird.jpg')
+    img = Image.open('../pics/ori.jpg')
     #plt.figure(figsize=(19.2, 10.8))
     #plt.imshow(img)
     #plt.show()
@@ -128,19 +165,19 @@ if __name__ == '__main__':
     npimg = np.array(img)
     #showGDimg(computeGD(npimg))
     #showGDimg(computeGDTorch(npimg).numpy())
-    for i in range(10):
-        computeGDTorch(npimg)
+    #for i in range(10):
+    #    computeGDTorch(npimg)
 
     #showGDimg(computeGD(npimg))
-    '''
+
     # plt.imshow(img.convert('L'))
     print('ready, ', time.asctime(time.localtime(time.time())))
-    for j in range(30):
+    for j in range(10):
         for i in range(10):
             npimg = deleteOneRow(npimg)
             print('finished', j * 10 + i + 1, ", ", time.asctime(time.localtime(time.time())))
     newimg=Image.fromarray(npimg)
-    newimg.save('./out/ori.jpg')
-    '''
+    newimg.save('../out/ori.jpg')
+
 
 
