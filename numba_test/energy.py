fbcfeba3759e5c6c705339eb63c9bef0766b8550
Sirui Lu <lsrcz@pku.edu.cn>
2018-05-24 14:16:09 +0800

    slightly modified gradient

--- numba_test/energy.py
+++ numba_test/energy.py
@@ -1,5 +1,8 @@
+from numba_test.config import *
+
 from PIL import Image
 import numpy as np
+
 import matplotlib.pyplot as plt
 plt.ion()
 
@@ -13,20 +16,27 @@ def computeGD(npimg):
     gdsum = np.zeros((x,y),dtype=np.uint32)
     for i in prange(x):
         for j in range(y):
-            a = 0
+            #a = 0
             for k in range(3):
-                pixel = npimg[i,j,k]
-                for m in range(i-1,i+2):
-                    for n in range(j-1,j+2):
+                pixel = npimg[i, j, k]
+                for m in range(i - 1, i + 2):
+                    for n in range(j - 1, j + 2):
                         if m != -1 and m != x and n != -1 and n != y and (m != i or n != j):
-                            gdsum[i,j] += np.abs(npimg[m,n,k]-pixel)
-                            a += 1
-            gdsum[i,j] *= (360 // a)
+                            gdsum[i, j] += np.abs(npimg[m, n, k] - pixel)
+                            #a += 1
+            if (i == 0 or i == x - 1) and (j == 0 or j == y - 1):
+                gdsum[i, j] *= 40
+            elif i == 0 or i == x - 1 or j == 0 or j == y - 1:
+                gdsum[i, j] *= 24
+            else:
+                gdsum[i, j] *= 15
+            # gdsum[i, j] *= (360 // a)
     return gdsum
 
 def showGDimg(gdimg):
     gdimg = gdimg.astype(np.float32)
-    plt.figure(figsize=(19.2, 10.8))
-    plt.title("gradient image")
-    plt.imshow(Image.fromarray((gdimg / gdimg.max() * 255).astype(np.uint8)))
-    plt.show()
+    if NEED_DISPLAY:
+        plt.figure(figsize=(19.2, 10.8))
+        plt.title("gradient image")
+        plt.imshow(Image.fromarray((gdimg / gdimg.max() * 255).astype(np.uint8)))
+        plt.show()
